6901cfa7839caa890bb5024506afb82568d5034c
User <kirilvasilev00@gmail.com>
2025-12-11 20:59:31 +0200

    Cause merge conflict 2

--- gitDemo/src/com/chavdar/Main.java
+++ gitDemo/src/com/chavdar/Main.java
@@ -3,8 +3,8 @@ package com.chavdar;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Git");
-        System.out.println("Now i can continue make changes to my project ;))"
+
         System.out.println("Now I can modify the remote copy of the repository.");
-        
+        System.out.println("Now i cause merge conflicts with the remote copy again!");
     }
 }
