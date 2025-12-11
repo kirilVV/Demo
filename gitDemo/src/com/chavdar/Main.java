8688a227e9e33b69688d8b6ed452cb9f761ce6f9
User <kirilvasilev00@gmail.com>
2025-12-11 20:40:36 +0200

    Updated

--- gitDemo/src/com/chavdar/Main.java
+++ gitDemo/src/com/chavdar/Main.java
@@ -3,5 +3,7 @@ package com.chavdar;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        System.out.println("Now I can continue make changes to my project :)");
     }
 }
